23448d17ea812a8b4b3153b0f88487246359b043
Kaloyan <kthristow@gmail.com>
2024-01-28 18:32:33 +0200

    Add more details components
    Add details components for titles and details for each meal

---
+++ MealsApp/components/MealDetail/Subtitle.js
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet } from "react-native";
+function Subtitle({ children }) {
+  return (
+    <View style={styles.subTitleContainer}>
+      <Text style={styles.subTitle}>{children}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  subTitle: {
+    color: "#e2b497",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  subTitleContainer: {
+    padding: 6,
+    marginHorizontal: 12,
+    marginVertical: 4,
+    borderBottomColor: "#e2b497",
+    borderBottomWidth: 2,
+  },
+});
+export default Subtitle;
